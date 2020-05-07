9580d9efef711f954d90cc434051859d72ec0416
Dinmukhamed Sailaubek <s.dinmuhamed@gmail.com>
2020-05-07 13:54:32 +0600

    Created selectors for input and button

---
+++ app.js
@@ -0,0 +1,7 @@
+// Create selectors for input and button
+
+const inputWithClass = document.querySelector("input.todo-input")
+console.log("INPUT with CLASS: ", inputWithClass)
+
+const buttonWithClass = document.querySelector("button.todo-button")
+console.log("BUTTON with CLASS: ", buttonWithClass)
